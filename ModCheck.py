1814f5667f7a24d92596f8c02954cd7ae3c872fe
Meera <76085037+pistacium@users.noreply.github.com>
2022-04-09 02:51:17 -0400

    better settings, tells user what was added/removed

--- ModCheck.py
+++ ModCheck.py
@@ -3,76 +3,86 @@ from os import listdir, mkdir, path, remove, rmdir
 from os.path import isfile, join
 import shutil
 import json
+import yaml
 
+exists = isfile('legalMods.yaml')
+if exists:
+    remove('legalMods.yaml')
 
-file = wget.download("https://github.com/pistacium/LegalMods/releases/download/mods/LegalMods.json")
-with open("LegalMods.json") as f:
-    mods = json.load(f)
-remove("LegalMods.json")
+file = wget.download("https://github.com/pistacium/LegalMods/releases/download/mods/legalMods.yaml")
+with open("legalMods.yaml") as f:
+    mods = yaml.safe_load(f)
+    mods = mods['1.16']
+remove("LegalMods.yaml")
 
+new = input("\nWould you like to create a new settings file ModCheckSettings.yaml? (y/n) ")
 
-new = input("\nWould you like to create a new settings file ModCheckSettings.txt? (y/n) ")
+file = 'ModCheckSettings.yaml'
+exists = isfile(file)
+
 print()
-if new.lower()=="y" or new.lower=="yes":
-    with open('ModCheckSettings.txt', mode='w') as f:
-        lines = ['pathToMods : Replace everything after the colon with the path to .minecraft\\mods. You may add multiple paths separated by spaces']
+if not exists or new.lower()=='y' or new.lower()=='yes':
+    if not exists:
+        tmp = input("Settings file not found, making new one. Press ENTER to continue.")
+        print()
+    with open('ModCheckSettings.yaml', 'w', encoding='utf8') as f:
+        data = {
+            'useMod' : {},
+            'directories' : [
+                'Replace with path to .minecraft//mods',
+                'Add multiple directories as such'                
+            ] 
+        }
         for mod in mods:
-            line = mod+" : True"
-            lines.append(line)
-        f.writelines('\n'.join(lines))
+            data['useMod'][mod] = True
+        yaml.dump(data, f, default_flow_style=False, allow_unicode=True)
 
-    print("Edit the settings file by adding your directories and changing whether you are using certain mods or not. Default is set to True, meaning it will use all legal mods.")
+    print("Edit the new settings file by adding your directories and changing whether you are using certain mods or not.")
+    print("Default is set to True, meaning it will use all legal mods.")
     done = input("Confirm that you are done configuring your settings by pressing ENTER.")
     print()
     
 
 else:
-    with open('ModCheckSettings.txt') as f:
-        lines = f.readlines()
-        lines = lines[1:]
+    with open('ModCheckSettings.yaml', 'r', encoding='utf8') as f:
+        datal = yaml.safe_load(f)
+        #lines = f.readlines()
+        #lines = lines[1:]
         mods1 = mods.keys()
-        mods2 = []
-        for line in lines:
-            line = line.split()[0]
-            mods2.append(line)
+        mods2 = datal['useMod'].keys()
+        #print(mods2)
         b1 = list(set(mods1).difference(mods2)) #Does our list of legal mods have more entries (i.e. mod got legalized)
         b2 = list(set(mods2).difference(mods1)) #Does our settings have too many mods          (i.e. mod got banned)
-    with open('ModCheckSettings.txt', 'a') as f:
+    with open('ModCheckSettings.yaml', 'w', encoding='utf8') as f:
         for legalized in b1:
             print(legalized, "has gotten legalized!")
             default = input("Would you like to use this mod, or not? (y/n) ")
             print()
             if default.lower()=='y' or default.lower()=='yes':
-                line = "\n"+legalized+" : True\n"
-                f.write(line)
+                datal['useMod'][legalized] = True
             else:
-                line = "\n"+legalized+" : False\n"
-                f.write(line)
+                datal['useMod'][legalized] = False
         for banned in b2:
             print(banned, "has been banned (maybe, or one of your settings is mislabeled). Please make sure to remove this mod from your directory and your settings.")
 
+        yaml.dump(datal, f, default_flow_style=False, allow_unicode=True)
+
+        
 dirs = []
 sets = {}
-with open('ModCheckSettings.txt') as f:
-    lines = f.readlines()
-    dirs = lines[0][13:]
-    dirs = dirs.split()
-    for line in lines[1:]:
-        i = line.find(':')
-        name = line[:i-1]
-        val = line[i+2:-1]
-        sets[name] = val
-        
+with open('ModCheckSettings.yaml', encoding='utf8') as f:
+    data = yaml.safe_load(f)    
+dirs = data['directories']
+sets = data['useMod']
+
+if not (path.exists("tmp")):
+    mkdir('tmp') 
 dl = {}
 for mod in mods:
-    #direc = path.abspath(__file__)+'\\tmp'
-    if not (path.exists("tmp")):
-        mkdir('tmp')
-    #print(mod, sets[mod])
-    if sets[mod]=="True" or sets[mod]=="Tru":
+    if sets[mod]:
         file = wget.download(mods[mod], out="tmp")
         dl[mod] = file
-    
+names={}
 for d in dirs:
     files = [f for f in listdir(d) if isfile(join(d, f))]
     for file in files:
@@ -81,19 +91,19 @@ for d in dirs:
     for mod in dl:
         shutil.copy(dl[mod], d)
 
-        dl[mod] = dl[mod][4:]
+        names[mod] = dl[mod][4:]
 
-    new = sorted(dl.values())
+    new = sorted(names.values())
     old = sorted(files)
 
     addeds = list(set(new).difference(old))
-    removeds = list(set(old).difference(new)))
+    removeds = list(set(old).difference(new))
     for added in addeds:
         print(added, "was added.")
     for removed in removeds:
         print(removed, "was removed.")
     
-    shutil.rmtree("tmp")
+shutil.rmtree("tmp")
 print()
 egg = input("\nWe should be done here, press ENTER to leave. if it didnt work talk to meera#8941.")
     
